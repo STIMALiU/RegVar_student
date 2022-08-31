b76c487b9b0fb1da57510f26172ef2df2aed32ba
josefwilzen <josefwilzen@gmail.com>
2022-08-31 15:48:48 +0200

    updates

--- tests/tasks/simple_lm_tests.R
+++ tests/tasks/simple_lm_tests.R
@@ -3,6 +3,7 @@ context("simple_lm()")
 
 test_that("simple_lm", {  
   
+  
   body_contain<-function(object,expected) {any(grepl(x = as.character(body(object)), pattern = expected))}
   package_loaded<-function(object){any(grepl(object, search()))}
   
@@ -21,12 +22,27 @@ test_that("simple_lm", {
                             info = "Fel: Argumenten i funktionen har felaktiga namn.")
   
   # testar argument
-  expect_error(simple_lm(x = 1:10,y = "hej"),"y is not numeric",
-               info="Fel: Funktionen ger inte rätt felmeddlende när y inte är numerisk")
-  expect_error(simple_lm(x = c(FALSE,TRUE),y = 3:39),"x is not numeric",
-               info="Fel: Funktionen ger inte rätt felmeddlende när x inte är numerisk")
-  expect_error(simple_lm(x = 1:20,y = 1:3),"not the same number of elements in x and y",
-               info="Fel: Funktionen ger inte rätt felmeddlende när x och y är olika långa")
+  # testar x:
+  expect_error(simple_lm(x = 1:130,y = FALSE),
+               info="Funktionen ger inte fel när y inte är numerisk")
+  expect_error(simple_lm(x = 1:10,y = "hej"),
+               info="Funktionen ger inte fel när y inte är numerisk")
+  expect_error(simple_lm(x = 1:10,y = "abc123"),"^y is not numeric$",
+               info="Fel: Funktionen ger inte rätt felmeddlende när y inte är numerisk: y is not numeric")
+  
+  # testar y:
+  expect_error(simple_lm(x = letters[1:3],y = 3:5),
+               info="Funktionen ger inte fel när x inte är numerisk")
+  expect_error(simple_lm(x = c(FALSE,TRUE),y = 3:39),
+               info="Funktionen ger inte fel när x inte är numerisk")
+  expect_error(simple_lm(x = c(FALSE,TRUE),y = 3:39),"^x is not numeric$",
+               info="Fel: Funktionen ger inte rätt felmeddlende när x inte är numerisk: x is not numeric")
+  
+  # testar lika längd på x och y:
+  expect_error(simple_lm(x = 1:200,y = 1:3),
+               info="Funktionen ger inte fel när x och y är olika långa")
+  expect_error(simple_lm(x = 1:20,y = 1:3),"^not the same number of elements in x and y$",
+               info="Fel: Funktionen ger inte rätt felmeddlende när x och y är olika långa: not the same number of elements in x and y")
   
   # testar output
   expect_true(is.list(simple_lm(x = 1:20,y = 3*(1:20)+4)),
@@ -34,6 +50,12 @@ test_that("simple_lm", {
   
   expect_equal(names(simple_lm(x = 1:20,y = -4*(1:20)-30)),c("beta0","beta1","sigma"),
                info="Fel: Funktionen returnerar inte en lista med rätt namn på elementen")
+  
+  # kollar förbjudna funktioner:
+  expect_false(body_contain(object = simple_lm,expected = "lm"),
+               info = "Fel: Funktionen lm() används i funktionen.")
+  expect_false(body_contain(object = simple_lm,expected = "%*%"),
+               info = "Fel: Funktionen %*% används i funktionen.")
   
   # data utan brus:
   # testar fallet: simple_lm(x = 1:20,y = 1:20+4)
@@ -53,63 +75,77 @@ test_that("simple_lm", {
   expect_equal( simple_lm(x = 1:30,y = -5*(1:30)-100)$sigma,0,
                 info="sigma är ej korrekt för simple_lm(x = 1:30,y = -5*(1:30)-100)")
   
-  # expect_error(simple_lm()(1:4),
-  #              info="Fel: Funktionen avbryter/stoppar inte om x har jämt antal element.")
-  # 
-  # expect_error(simple_lm()(1:4),"x not of odd length.",
-  #              info="Fel: Funktionen ger inte rätt felmeddlende när x har jämt antal element.")
-  # 
-  # expect_true(is.matrix(simple_lm()(1:3)),
-  #             info="Fel: Funktionen returnerar inte en matrix")
-  # 
-  # expect_equal(dim(simple_lm()(1:5)), c(3,3),
-  #              info="Fel: Funktionen returnerar en matrix med fel dimension")
-  # 
-  # expect_equal(dim(simple_lm()(1)), c(1,1),
-  #              info="Fel: Funktionen returnerar en matrix med fel dimension för simple_lm()(x=1)")
-  # 
-  # expect_equal(simple_lm()(1:9)[3:4,5], c(3,2),
-  #              info="Fel: Funktionen returnerar en felaktig matris med simple_lm()(1:9)")
-  # 
-  # expect_equal(simple_lm()(1:11)[6,6], c(1),
-  #              info="Fel: Funktionen returnerar en felaktig matris med simple_lm()(1:11)")
-  # 
-  # expect_equal(simple_lm()(1:11)[1,6], c(6),
-  #              info="Fel: Funktionen returnerar en felaktig matris med simple_lm()(1:11)")
-  # 
-  # expect_equal(simple_lm()(1:11)[6,1], c(11),
-  #              info="Fel: Funktionen returnerar en felaktig matris med simple_lm()(1:11)")
-  # 
-  # expect_equal(simple_lm()(1:5), test1,
-  #              info="Fel: Funktionen returnerar en felaktig matris med simple_lm()(1:5)")
-  # expect_equal(simple_lm()(c("a","b","c")), test2,
-  #              info="Fel: Funktionen returnerar en felaktig matris med simple_lm()(c(\"a\",\"b\",\"c\"))")
-  # expect_equal(simple_lm()(c(11,22,33,44,22,33,44)), test3,
-  #              info="Fel: Funktionen returnerar en felaktig matris med simple_lm()(c(11,22,33,44,22,33,44))")
-  # expect_equal(simple_lm()(x = c(1:7,2:7)), test4,
-  #              info="Fel: Funktionen returnerar en felaktig matris med simple_lm()(x=c(1:7,2:7))")
-  # 
-  # expect_function_code(object = simple_lm(), expected = "return", 
-  #                      info = "Fel: return() saknas i funktionen.") 
-  # 
-  # expect_false(body_contain(object = simple_lm(),expected = "toeplitz"),
-  #              info = "Fel: Funktionen toeplitz() används i funktionen.")
-  # 
-  # expect_false(body_contain(object = simple_lm(),expected = "toeplitz.spam"),
-  #              info = "Fel: Funktionen toeplitz.spam() används i funktionen.")
-  # 
-  # expect_false(body_contain(object = simple_lm(),expected = "Toeplitz"),
-  #              info = "Fel: Funktionen Toeplitz() används i funktionen.")
-  # 
-  # expect_false(body_contain(object = simple_lm(),expected = "spam"),
-  #              info = "Fel: Funktioner ur paketet spam används i funktionen.")
-  # 
-  # expect_false(body_contain(object = simple_lm(),expected = "ts"),
-  #              info = "Fel: Funktioner ur paketet ts används i funktionen.")
-  # 
-  # expect_false(body_contain(object = simple_lm(),expected = "pracma"),
-  #              info = "Fel: Funktioner ur paketet pracma används i funktionen.")
-  # 
-  # expect_silent(object = simple_lm()(x=c(1,2,3)))
+  # testar fallet: simple_lm(x = 1:30,y = 0*(1:30)-10)
+  expect_equal( simple_lm(x = 1:30,y = 0*(1:30)-10)$beta1,0,
+                info="beta1 är ej korrekt för simple_lm(x = 1:30,y = 0*(1:30)-10)")
+  expect_equal( simple_lm(x = 1:30,y = 0*(1:30)-10)$beta0,-10,
+                info="beta0 är ej korrekt för simple_lm(x = 1:30,y = 0*(1:30)-10)")
+  expect_equal( simple_lm(x = 1:30,y = 0*(1:30)-10)$sigma,0,
+                info="sigma är ej korrekt för simple_lm(x = 1:30,y = 0*(1:30)-10)")
+  
+  
+  # testar fallet: simple_lm(x = 1:30,y = 20*(1:30)+0)
+  expect_equal( simple_lm(x = 1:30,y = 20*(1:30)+0)$beta1,20,
+                info="beta1 är ej korrekt för simple_lm(x = 1:30,y = 20*(1:30)+0)")
+  expect_equal( simple_lm(x = 1:30,y = 20*(1:30)+0)$beta0,0,
+                info="beta0 är ej korrekt för simple_lm(x = 1:30,y = 20*(1:30)+0)")
+  expect_equal( simple_lm(x = 1:30,y = 20*(1:30)+0)$sigma,0,
+                info="sigma är ej korrekt för simple_lm(x = 1:30,y = 20*(1:30)+0)")
+  
+  
+  
+  
+  # fallet: beta1 = 0 och beta0 = 0, men sigma>0
+  set.seed(42)
+  temp_list1<-simple_lm(x = 1:5000,y = rnorm(5000))
+  
+  expect_equal( temp_list1$beta1,1.823625e-06,tolerance=1e-6,
+                info="beta1 är ej korrekt för set.seed(42); temp_list1<-simple_lm(x = 1:5000,y = rnorm(5000))")
+  expect_equal( temp_list1$beta0,-0.01895303,tolerance=1e-6,
+                info="beta0 är ej korrekt för set.seed(42); temp_list1<-simple_lm(x = 1:5000,y = rnorm(5000))")
+  expect_equal( temp_list1$sigma,1.005041,tolerance=1e-6,
+                info="sigma är ej korrekt för set.seed(42); temp_list1<-simple_lm(x = 1:5000,y = rnorm(5000))")
+  rm(temp_list1)
+  
+  
+  # fler vanliga fall:
+  # trees data
+  temp_list2<-simple_lm(x = trees$Girth,y = trees$Volume) 
+  expect_equal( temp_list2$beta1,5.065856,tolerance=1e-4,
+                info="beta1 är ej korrekt för simple_lm(x = trees$Girth,y = trees$Volume)")
+  expect_equal( temp_list2$beta0,-36.94346,tolerance=1e-4,
+                info="beta0 är ej korrekt för simple_lm(x = trees$Girth,y = trees$Volume)")
+  expect_equal( temp_list2$sigma,4.251988,tolerance=1e-4,
+                info="sigma är ej korrekt för simple_lm(x = trees$Girth,y = trees$Volume)")
+  rm(temp_list2)
+  
+  
+  temp_list3<-simple_lm(x = trees$Height,y = trees$Volume) 
+  expect_equal( temp_list3$beta1,1.54335,tolerance=1e-4,
+                info="beta1 är ej korrekt för simple_lm(x = trees$Height,y = trees$Volume)")
+  expect_equal( temp_list3$beta0,-87.12361,tolerance=1e-4,
+                info="beta0 är ej korrekt för simple_lm(x = trees$Height,y = trees$Volume)")
+  expect_equal( temp_list3$sigma,13.39698,tolerance=1e-4,
+                info="sigma är ej korrekt för simple_lm(x = trees$Height,y = trees$Volume)")
+  rm(temp_list3)
+  
+  # cars data:
+  temp_list4<-simple_lm(x = cars$speed,y = cars$dist) 
+  expect_equal( temp_list4$beta1,3.932409,tolerance=1e-4,
+                info="beta1 är ej korrekt för simple_lm(x = cars$speed,y = cars$dist)")
+  expect_equal( temp_list4$beta0,-17.57909,tolerance=1e-4,
+                info="beta0 är ej korrekt för simple_lm(x = cars$speed,y = cars$dist)")
+  expect_equal( temp_list4$sigma,15.37959,tolerance=1e-4,
+                info="sigma är ej korrekt för simple_lm(x = cars$speed,y = cars$dist)")
+  rm(temp_list4)
+  
+  temp_list5<-simple_lm(x = cars$speed,y = -1*cars$dist+100)  
+  expect_equal( temp_list5$beta1, -3.932409,tolerance=1e-4,
+                info="beta1 är ej korrekt för simple_lm(x = cars$speed,y = -1*cars$dist+100)")
+  expect_equal( temp_list5$beta0,117.5791,tolerance=1e-4,
+                info="beta0 är ej korrekt för simple_lm(x = cars$speed,y = -1*cars$dist+100)")
+  expect_equal( temp_list5$sigma,15.37959,tolerance=1e-4,
+                info="sigma är ej korrekt för simple_lm(x = cars$speed,y = -1*cars$dist+100)")
+  rm(temp_list5)
   
 })
