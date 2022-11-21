6b2704a8d7ca6935b51f5a96c91101651e68d3fc
josefwilzen <josefwilzen@gmail.com>
2022-11-21 11:35:24 +0100

    updates

--- lab12_code/sim_anova_func.R
+++ lab12_code/sim_anova_func.R
@@ -16,6 +16,8 @@ sim_anova_func<-function(ver,n_obs=200,seed1=235,seed2=4655){
     reg_beta<-c(25,0,0,7,5,-10)
   }else if(ver==5){
     reg_beta<-c(25,2,-1,1.5,0.5,-0.5)
+  }else{
+    stop("ver= must be om of: 1 2 3 4 5")
   }
   set.seed(seed2)
   y<-X%*%reg_beta+rnorm(n = nrow(X),sd=4)
